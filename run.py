070bf2c076c479cdf874a0061e6e37ad0ec54b01
goserizz <goserizz@gmail.com>
2021-02-22 23:22:10 +0800

    first commit

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ check_time = [0, 4, 8, 12, 16, 20]
 while True:
     time.sleep(1)
     now_time = time.localtime()
-    if now_time.tm_hour in check_time and now_time.tm_sec == 0:
+    if now_time.tm_hour in check_time and now_time.tm_min == 0 and now_time.tm_sec == 0:
         os.system("python3 mining_data.py")
         os.system("git add *")
         os.system('git commit -m "auto commit"')
