2a0cb4ad9677fd6d7eae7fdba78c12146e61963a
root <root@vultr.guest>
2021-03-02 22:06:32 +0800

    first commit

--- run.py
+++ run.py
@@ -3,19 +3,15 @@ import time
 
 
 check_time = [0, 4, 8, 12, 16, 20]
-now_time = time.localtime().tm_hour
-for temp_time in [4, 8, 12, 16, 20, 24]:
-    if now_time < temp_time:
-        next_time = temp_time
-if next_time == 24:
-    next_time = 0
+last_time = time.localtime().tm_hour
 
 while True:
     time.sleep(60)
-    now_time = time.tm_hour
-    if now_time == next_time:
-        os.system("python3 mining_data.py")
-        os.system("git add *")
-        os.system('git commit -m "auto commit"')
-        os.system("git push -u origin main")
-        next_time = (next_time + 4) % 24
+    now_time = time.localtime().tm_hour
+    if now_time != last_time:
+        if now_time in check_time:
+            os.system("python3 mining_data.py")
+            os.system("git add *")
+            os.system('git commit -m "auto commit"')
+            os.system("git push -u origin main")
+        last_time = now_time
