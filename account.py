4402ee493545a487d112357163315671f619abd1
root <root@vultr.guest>
2021-03-08 14:43:15 +0800

    balance update

--- account.py
+++ account.py
@@ -11,6 +11,88 @@ def log(log_info, datas):
         f.writelines('Now Balance: ' + str(datas) + '\n')
 
 
+def save(datas):
+    with open('account.json', mode='w') as f:
+        json.dump(datas, f)
+    os.system("git add account.*")
+    os.system('git commit -m "balance update"')
+    os.system("git push -u origin main")
+
+
+def clear(user, note):
+    with open('account.json', mode='r') as f:
+        datas = json.load(f)
+    if user:
+        datas[user] = 0
+        info = "User {}'s balance is set to 0.".format(user)
+        print(info)
+        log(info, datas)
+    else:
+        for user in datas.keys():
+            datas[user] = 0
+            info = "User {}'s balance is set to 0. ({})".format(user, note)
+            print(info)
+            log(info, datas)
+    save(datas)
+
+
+def deposit(user, amount, note):
+    with open('account.json', mode='r') as f:
+        datas = json.load(f)
+    if user not in datas.keys():
+        print("No {} Found.".format(user))
+        exit()
+    amount = args.amount
+    if amount <= 0:
+        print("Invalid ETH Amount.")
+        exit()
+    datas[user] += amount
+    info = "User {} deposits {} (now {}) ETH. ({})".format(user, amount, datas[user], note)
+    print(info)
+    log(info, datas)
+    save(datas)
+
+
+def withdraw(user, amount, note):
+    if user not in datas.keys():
+        print("No {} Found.".format(user))
+        exit()
+    if amount <= 0:
+        print("Invalid ETH Amount.")
+        exit()
+    if amount > datas[user]:
+        print("Insufficient Funds.")
+        exit()
+    datas[user] -= amount
+    info = "User {} withdraws {} (now {}) ETH. ({})".format(user, amount, datas[user], note)
+    print(info)
+    log(info, datas)
+    save(datas)
+
+
+def sell(amount, note):
+    tot_amount = 0
+    for user in datas.keys():
+        tot_amount += datas[user]
+    tot_remain = tot_amount - amount
+    for user in datas.keys():
+        datas[user] = datas[user] / tot_amount * tot_remain
+    info = "Sells {} ETH. Remains {} ETH now. ({})".format(amount, tot_remain, note)
+    print(info)
+    log(info, datas)
+    save(datas)
+
+
+def show_info():
+    tot_amount = 0
+    for user in datas.keys():
+        tot_amount += datas[user]
+    print("Total Amount: {:.3f} mETH.".format(tot_amount * 1000))
+    print("Name\tAmount/mETH\tShare/%")
+    for user in datas.keys():
+        print("{}\t{:.3f}\t\t{:.2f}".format(user, datas[user] * 1000, datas[user] / tot_amount * 100))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Account Management")
     parser.add_argument('operation', type=str, help="Performing Operation: clear | deposit | withdraw | info | sell")
@@ -19,75 +101,16 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--note', type=str, help="Additional Description")
     args = parser.parse_args()
 
-    with open('account.json', mode='r') as f:
-        datas = json.load(f)
     if args.operation == 'clear':
-        if args.user:
-            datas[args.user] = 0
-            info = "User {}'s balance is set to 0.".format(args.user)
-            print(info)
-            log(info, datas)
-        else:
-            for user in datas.keys():
-                datas[user] = 0
-                info = "User {}'s balance is set to 0. ({})".format(user, args.note)
-                print(info)
-                log(info, datas)
+        clear(args.user, args.note)
     elif args.operation == 'deposit':
-        user = args.user
-        if user not in datas.keys():
-            print("No {} Found.".format(user))
-            exit()
-        amount = args.amount
-        if amount <= 0:
-            print("Invalid ETH Amount.")
-            exit()
-        datas[user] += amount
-        info = "User {} deposits {} (now {}) ETH. ({})".format(user, amount, datas[user], args.note)
-        print(info)
-        log(info, datas)
+        clear(args.user, args.amount, args.note)
     elif args.operation == 'withdraw':
-        user = args.user
-        if user not in datas.keys():
-            print("No {} Found.".format(user))
-            exit()
-        amount = args.amount
-        if amount <= 0:
-            print("Invalid ETH Amount.")
-            exit()
-        if amount > datas[user]:
-            print("Insufficient Funds.")
-            exit()
-        datas[user] -= amount
-        info = "User {} withdraws {} (now {}) ETH. ({})".format(user, amount, datas[user], args.note)
-        print(info)
-        log(info, datas)
+        withdraw(args.user, args.amount, args.note)
     elif args.operation == 'info':
-        tot_amount = 0
-        for user in datas.keys():
-            tot_amount += datas[user]
-        print("Total Amount: {:.3f} mETH.".format(tot_amount * 1000))
-        print("Name\tAmount/mETH\tShare/%")
-        for user in datas.keys():
-            print("{}\t{:.3f}\t\t{:.2f}".format(user, datas[user] * 1000, datas[user] / tot_amount * 100))
+        show_info()
     elif args.operation == 'sell':
-        amount = args.amount
-        tot_amount = 0
-        for user in datas.keys():
-            tot_amount += datas[user]
-        tot_remain = tot_amount - amount
-        for user in datas.keys():
-            datas[user] = datas[user] / tot_amount * tot_remain
-        info = "Sells {} ETH. Remains {} ETH now. ({})".format(amount, tot_remain, args.note)
-        print(info)
-        log(info, datas)
+        sell(args.amount, args.note)
     else:
         exit()
-    
-    if args.operation != 'info':
-        with open('account.json', mode='w') as f:
-            json.dump(datas, f)
-        os.system("git add account.*")
-        os.system('git commit -m "balance update"')
-        os.system("git push -u origin main")
         
