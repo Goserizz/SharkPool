0979db4239f14a97e9520043042b6762cc726749
root <root@vultr.guest>
2021-03-13 12:52:43 +0800

    first commit

--- run.py
+++ run.py
@@ -10,8 +10,14 @@ while True:
     now_time = time.localtime().tm_hour
     if now_time != last_time:
         if now_time in check_time:
-            os.system("python3 mining_data.py")
-            os.system("git add *")
-            os.system('git commit -m "auto commit"')
-            os.system("git push -u origin main")
+            while True:
+                os.system("python3 mining_data.py")
+                with open('log', mode='r') as f:
+                    lines = f.readlines()
+                if int(lines[-1][12:14]) == now_time:
+                    os.system("git add *")
+                    os.system('git commit -m "auto commit"')
+                    os.system("git push")
+                    break
+                time.sleep(1)
         last_time = now_time
